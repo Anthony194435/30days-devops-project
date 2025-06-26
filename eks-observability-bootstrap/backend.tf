e6c656aedc9ff6387c5a81efc3e0c7dc680821a0
Anthony194435 <iyke.anthony980@gmail.com>
2025-06-26 05:53:14 -0500

    Added Jenkinsfile

--- eks-observability-bootstrap/backend.tf
+++ eks-observability-bootstrap/backend.tf
@@ -1,9 +1,9 @@
-terraform {  
-  backend "s3" {  
-    bucket       = "your-terraform-state-bucket"  
-    key          = "path/to/your/statefile.tfstate"  
-    region       = "us-east-1"  
-    encrypt      = true  
-    use_lockfile = true  #S3 native locking
-  }  
+terraform {
+  backend "s3" {
+    bucket         = "ikechukwu-terraform-state-file"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
 }
