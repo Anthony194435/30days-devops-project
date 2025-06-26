2fe7bf48c0e6cd5d2d29174b282df3dfb6c4eda3
Anthony194435 <iyke.anthony980@gmail.com>
2025-06-26 07:14:57 -0500

    Edited main.tf

--- terraform-modules-infra/eks/main.tf
+++ terraform-modules-infra/eks/main.tf
@@ -5,20 +5,34 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.private_subnet_ids
+  vpc_id     = module.vpc.vpc_id
 
   enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access           = true
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3.medium"]
+    disk_size      = 20
+  }
 
   eks_managed_node_groups = {
     default = {
       desired_size = 2
-      max_size     = 3
       min_size     = 1
-      instance_types = ["t3.medium"]
+      max_size     = 3
     }
   }
 
-  tags = var.tags
+  cluster_addons = {
+    kube-proxy = {}
+    coredns    = {}
+    vpc-cni    = {}
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
