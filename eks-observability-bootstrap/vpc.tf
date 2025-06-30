3c792b74ee88b489d7c7734c4110f2ef4ca601a1
Anthony194435 <iyke.anthony980@gmail.com>
2025-06-30 08:39:41 -0500

    deleted terraform files

--- eks-observability-bootstrap/vpc.tf
+++ eks-observability-bootstrap/vpc.tf
@@ -1,7 +1,10 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "eks-vpc"
   }
@@ -9,6 +12,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
   tags = {
     Name = "eks-igw"
   }
@@ -18,8 +22,9 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
   tags = {
     Name = "eks-public-subnet-${count.index + 1}"
   }
@@ -27,6 +32,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
   tags = {
     Name = "eks-public-rt"
   }
@@ -44,3 +50,26 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_security_group" "eks" {
+  name        = "eks-cluster-sg"
+  description = "Allow all EKS traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-sg"
+  }
+}
