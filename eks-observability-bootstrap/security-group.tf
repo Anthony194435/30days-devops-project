9cbcaf7410861e031b4094997b65b85d128a02d6
Anthony194435 <iyke.anthony980@gmail.com>
2025-06-30 08:01:58 -0500

    deleted terraform files

--- eks-observability-bootstrap/security-group.tf
+++ eks-observability-bootstrap/security-group.tf
@@ -1,29 +1,22 @@
-
 resource "aws_security_group" "worker-sg" {
-  name_prefix = "all_worker_management"
+  name_prefix = "worker-sg"
   vpc_id      = module.vpc.vpc_id
-}
 
-resource "aws_security_group_rule" "worker-sg-ingress" {
-  description       = "allow inbound traffic from eks"
-  from_port         = 0
-  protocol          = "-1"
-  to_port           = 0
-  security_group_id = aws_security_group_rule.worker-sg_egress
-  type              = "ingress"
-  cidr_blocks = [
-    "10.0.0.0/8",
-    "172.16.0.0/12",
-    "192.168.0.0/16",
-  ]
-}
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  }
 
-resource "aws_security_group_rule" "worker-sg_egress" {
-  description       = "allow outbound traffic to anywhere"
-  from_port         = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group_rule.worker-sg-ingress
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "worker-sg"
+  }
 }
